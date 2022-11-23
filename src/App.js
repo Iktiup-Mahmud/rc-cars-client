e348dc1c37d5fb8ee3b54be4d1f5bd8fb68706c9
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-11-23 22:39:57 +0600

    'basic-navbar-added'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Header></Header>
     </div>
   );
 }
